c349ea60bf709541d6b5724e207ac7a9cc9c97c4
czw <543481852@qq.com>
2016-08-17 18:01:51 +0800

    a commit 2016-08-17 end

--- app/src/main/java/com/xht/android/companyhelp/SheBaoSActivity.java
+++ app/src/main/java/com/xht/android/companyhelp/SheBaoSActivity.java
@@ -2,26 +2,38 @@ package com.xht.android.companyhelp;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.xht.android.companyhelp.model.PersonOfSheBao;
+import com.xht.android.companyhelp.net.APIListener;
+import com.xht.android.companyhelp.net.VolleyHelpApi;
+
+import java.util.ArrayList;
+
 /**
  * Created by Administrator on 2016/8/16.
  * 社保服务
  */
-public class SheBaoSActivity extends Activity {
+public class SheBaoSActivity extends Activity implements View.OnClickListener {
 
     private static final String TAG = "SheBaoSActivity";
 
     private int mUId;
+    private ArrayList<PersonOfSheBao> mArrayList = new ArrayList<>();
+    private PersonOfSheBaoAdapter mPersonOfSheBaoAdapter;
 
+    private ProgressDialog mProgDoal;
     private Spinner mCompNameSpinner;
     private ImageButton mAddItemIBtn;
     private ListView mListView;
@@ -42,7 +54,11 @@ public class SheBaoSActivity extends Activity {
                 new ActionBar.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         int change = ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_SHOW_CUSTOM;
         aBar.setDisplayOptions(change);
+        mArrayList.add(new PersonOfSheBao());
         initView();
+        mAddItemIBtn.setOnClickListener(this);
+        mPersonOfSheBaoAdapter = new PersonOfSheBaoAdapter(mArrayList);
+        mListView.setAdapter(mPersonOfSheBaoAdapter);
     }
 
     private void initView() {
@@ -64,5 +80,75 @@ public class SheBaoSActivity extends Activity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * 根据用户id获取公司列表和社保价格
+     */
+    private void getComListAndJiaGe(int uid) {
+        createProgressDialog("价格获取中...");
+        VolleyHelpApi.getInstance().getComListAndJiaGeofYeWu(uid, new APIListener() {
+            @Override
+            public void onResult(Object result) {
+
+            }
+
+            @Override
+            public void onError(Object e) {
+                dismissProgressDialog();
+                App.getInstance().showToast(e.toString());
+                finish();
+            }
+        });
+    }
+
+    private void createProgressDialog(String title) {
+        if (mProgDoal == null) {
+            mProgDoal = new ProgressDialog(this);
+        }
+        mProgDoal.setTitle(title);
+        mProgDoal.setIndeterminate(true);
+        mProgDoal.setCancelable(false);
+        mProgDoal.show();
+    }
+
+    /**
+     * 隐藏mProgressDialog
+     */
+    private void dismissProgressDialog()
+    {
+        if(mProgDoal != null)
+        {
+            mProgDoal.dismiss();
+            mProgDoal = null;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_item:
+                mArrayList.add(new PersonOfSheBao());
+                mPersonOfSheBaoAdapter.notifyDataSetChanged();
+                break;
+        }
+    }
+
+    private class PersonOfSheBaoAdapter extends ArrayAdapter<PersonOfSheBao> {
+
+        public PersonOfSheBaoAdapter(ArrayList<PersonOfSheBao> items) {
+            super(SheBaoSActivity.this, 0, items);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.personshebao_item, parent, false);
+            }
+            PersonOfSheBao item = getItem(position);
+
+            return convertView;
+        }
+
     }
 }
---
+++ app/src/main/java/com/xht/android/companyhelp/model/PersonOfSheBao.java
@@ -0,0 +1,27 @@
+package com.xht.android.companyhelp.model;
+
+/**
+ * Created by Administrator on 2016/8/17.
+ * 社保业务里的人对象
+ */
+public class PersonOfSheBao {
+
+    private String mIdCard; //身份证号
+    private String mName;
+
+    public String getmIdCard() {
+        return mIdCard;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmIdCard(String mIdCard) {
+        this.mIdCard = mIdCard;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+}
